ab3550acd984418cf21c3e5c7b58d2ae40d301a4
Helder Paraense <heru.p.serra@gmail.com>
2021-11-07 14:54:27 +0000

    Linkedin inserted

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/Heruderu" target="_blank" rel="noreferrer">Helder Paraense</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/helder-paraense-4145ba42/" target="_blank" rel="noreferrer">Helder Paraense</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
